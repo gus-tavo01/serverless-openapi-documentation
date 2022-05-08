d7500ad23e67bd3cfcf745a6bd03524395c31ef8
gus-tavo01 <gustavoa.loera01@gmail.com>
2022-05-08 11:56:49 -0700

    modify clean script

--- scripts/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Clean
-cd build
-rm package.json
--- scripts/prepareBuild.js
+++ scripts/prepareBuild.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 
 function main() {
+  fs.rm('build/package.json', (err) => {
+    if (err) {
+      console.log('Error: ', err);
+    }
+  });
+  
   fs.copyFile('package.json', 'build/package.json', (err) => {
     if (err) {
       console.log('Error: ', err);
